b56552628d7053bc31edb10d923c6f80575f6c8f
khairunnisaalallah <124749527+khairunnisaalallah@users.noreply.github.com>
2023-10-24 19:44:26 +0700

    add home, education, time, profile fragment

---
+++ app/src/main/java/com/example/myapplication1/FragmentActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication1;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+
+    HomeFragment homeFragment = new HomeFragment();
+    EducationFragment educationFragment = new EducationFragment();
+    TimeFragment timeFragment = new TimeFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.time);
+        badgeDrawable.setVisible(true);
+        badgeDrawable.setNumber(8);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.education) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, educationFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.time) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, timeFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.profile) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
+}
