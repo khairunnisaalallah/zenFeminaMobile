5cf27e30a5b62ff9c9b635f3a621d0d71abfd786
khairunnisaalallah <124749527+khairunnisaalallah@users.noreply.github.com>
2023-11-02 10:00:26 +0700

    update interface

--- app/src/main/java/com/example/myapplication1/LoginActivity.java
+++ app/src/main/java/com/example/myapplication1/LoginActivity.java
@@ -4,21 +4,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class LoginActivity extends AppCompatActivity { //ini berubah
+public class LoginActivity extends AppCompatActivity {
+    private EditText username, sandi;
+    private Button masuk;
+    private String tusername = "admin";
+    private String tsandi = "Admin123";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button loginButton = findViewById(R.id.buttonlogin2);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+
+        username = findViewById(R.id.username);
+        sandi = findViewById(R.id.sandi);
+        masuk = findViewById(R.id.masuk);
+
+        masuk.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equalsIgnoreCase(tusername)&&sandi.getText().toString().equalsIgnoreCase(tsandi)) {
+                    Intent masuk = new Intent(LoginActivity.this, FragmentActivity.class);
+                    startActivity(masuk);
+
+                    Toast.makeText(LoginActivity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(LoginActivity.this, "Username atau sandi yang anda masukkan salah", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            {
+
+            }
+
+    });
+
+
+       /* Button masukButton = findViewById(R.id.masuk);
+        masukButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Arahkan ke LoginActivity
@@ -26,6 +58,9 @@ public class LoginActivity extends AppCompatActivity { //ini berubah
                 startActivity(intent);
             }
         });
+        */
+
+
         // Inisialisasi TextView
         TextView registerTextView = findViewById(R.id.textViewregis);
         // Menambahkan OnClickListener ke TextView
