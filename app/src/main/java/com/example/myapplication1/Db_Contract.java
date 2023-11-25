a15b67bdb2d8131e15f81527f4ce8691c8192848
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-11-25 11:35:44 +0700

    fix: api using domain, login &register

--- app/src/main/java/com/example/myapplication1/Db_Contract.java
+++ app/src/main/java/com/example/myapplication1/Db_Contract.java
@@ -2,8 +2,8 @@ package com.example.myapplication1;
 
 public class Db_Contract {
 
-    public static String ip ="192.168.1.7";
-    public static String urlLogin="http://"+ip+"/login-registration-android/login.php";
-    public static String urlRegister="http://"+ip+"/login-registration-android/register.php";
+    public static String ip ="zenfemina.com";
+    public static String urlLogin="http://"+ip+"/loginAPI";
+    public static String urlRegister="http://"+ip+"/registerAPI";
     public static String urlLogout="http://"+ip+"/login-registration-android/logout.php";
 }
