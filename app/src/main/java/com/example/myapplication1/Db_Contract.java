8a303113d722ed5e5b0bf194d1fc8a4ee2c5be3e
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2023-11-28 09:25:14 +0700

    add: verifications api

--- app/src/main/java/com/example/myapplication1/Db_Contract.java
+++ app/src/main/java/com/example/myapplication1/Db_Contract.java
@@ -5,5 +5,8 @@ public class Db_Contract {
     public static String ip ="zenfemina.com";
     public static String urlLogin="http://"+ip+"/loginAPI";
     public static String urlRegister="http://"+ip+"/registerAPI";
+    public static String urlEmailVerification = "http://"+ip+"/emailVerification";
+    public static String urlOtpVerification = "http://"+ip+"/otpVerification";
+    public static String urlNewPassword = "http://"+ip+"/newPassword";
     public static String urlLogout="http://"+ip+"/login-registration-android/logout.php";
 }
