90fd9c667747330127b52bfef3e61dfdbb317aad
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2024-01-13 20:53:49 +0700

    fix: calender and notif icon on dashboard

--- app/src/main/java/com/example/myapplication1/HomeFragment.java
+++ app/src/main/java/com/example/myapplication1/HomeFragment.java
@@ -22,8 +22,10 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -46,6 +48,7 @@ public class HomeFragment extends Fragment {
     SharedPreferences sharedPreferences;
 
     public static String cycleHistory_id;
+    public String StartDateEst, StartDateHist, EndDateEst, EndDateHist;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,13 +57,16 @@ public class HomeFragment extends Fragment {
 
         tokenManager token = new tokenManager(getContext());
 
+        //get history and get Est
+        getHistory();
+        getEst();
+
 
         // Mengambil referensi ke ImageButton
-        ImageButton imageButtonCalendar = view.findViewById(R.id.imageButtoncalender);
+        RelativeLayout imageButtonCalendar = view.findViewById(R.id.imageButtoncalender);
         RelativeLayout imageButtonQadha = view.findViewById(R.id.btnhutangsholat);
         RelativeLayout imageButtonAwali = view.findViewById(R.id.imageButtonAwali);
-
-        ImageButton imageButtonNotif = view.findViewById(R.id.imageButtonNotifikasi);
+        RelativeLayout imageButtonNotif = view.findViewById(R.id.imageButtonNotifikasi);
 
         imageButtonNotif.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,166 +104,7 @@ public class HomeFragment extends Fragment {
             }
         });
 
-
-
-
-        // Mengambil referensi ke TextView
-        firstDateTextView = view.findViewById(R.id.firstdate2);
-        lastDateTextView = view.findViewById(R.id.lastdate2);
-        sharedPreferences = requireActivity().getSharedPreferences("MyAppName", MODE_PRIVATE);
-        RequestQueue queue = Volley.newRequestQueue(requireActivity());
-        String url = Db_Contract.urlgetEst + "?token=" + token.getToken();
-        StringRequest profileRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    JSONArray jsonArray = jsonObject.getJSONArray("data");
-
-                    if (jsonArray.length() > 0) {
-                        JSONObject dataObject = jsonArray.getJSONObject(0);
-                        String startdate = dataObject.optString("start_date", ""); // Menggunakan optString untuk mendapatkan string kosong jika nilai "start_date" null
-                        String startdatenew = convertDateFormat(startdate.substring(0,10));
-
-                        String enddate = dataObject.optString("end_date", "");
-
-                        if (!startdate.isEmpty() ) {
-                            lastDateTextView.setText(startdatenew);
-
-                            // Setelah menampilkan data profil, lakukan permintaan ke getEstimation di sini
-                            try {
-                                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-
-                                Date startDate = sdf.parse(startdate);
-                                Date endDate = sdf.parse(enddate);
-                                Date currentDate = new Date();
-
-
-                                long diffInMillisBegin = startDate.getTime() - currentDate.getTime();
-                                long beginCycle = diffInMillisBegin / (24 * 60 * 60 * 1000);
-
-                                long diffInMillisEnd = endDate.getTime() - currentDate.getTime();
-                                long endCycle = diffInMillisEnd / (24 * 60 * 60 * 1000);
-
-                                String days = null;
-                                String nextWords = null;
-                                String cycleWords = null;
-                                if(startDate.after(currentDate)){
-                                    days = beginCycle + " Hari Lagi";
-                                    nextWords = "menuju periode Haid selanjutnya";
-                                    cycleWords = "Siklus selanjutnya";
-                                } else {
-                                    days = endCycle + " Hari lagi";
-                                    nextWords = "Kemungkinan periode Haid berakhir";
-                                    cycleWords = "Siklus saat ini";
-                                }
-
-                                TextView hariTextView = view.findViewById(R.id.hari);
-                                TextView nextTextView = view.findViewById(R.id.menujusiklus);
-                                TextView lastDateWords = view.findViewById(R.id.lastdateWords);
-                                hariTextView.setText(days);
-                                nextTextView.setText(nextWords);
-                                lastDateWords.setText(cycleWords);
-
-                            } catch (ParseException e) {
-                                e.printStackTrace();
-                                Toast.makeText(getContext(), "Error parsing date", Toast.LENGTH_SHORT).show();
-                            }
-
-                            // Setelah menampilkan data profil, lakukan permintaan ke getEstimation di sini
-                            String urlEstimation = Db_Contract.urlgetEst + "?token=" + token.getToken();
-                            StringRequest estimationRequest = new StringRequest(Request.Method.GET, urlEstimation, new Response.Listener<String>() {
-                                @Override
-                                public void onResponse(String responseEstimation) {
-                                    try {
-                                        JSONObject jsonObjectEstimation = new JSONObject(responseEstimation);
-
-                                        // Proses data estimasi di sini
-                                        // ...
-
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                        Toast.makeText(getContext(), "Error parsing Estimation JSON", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-
-
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    error.printStackTrace();
-                                    Toast.makeText(getContext(), "Error in Estimation request: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                                }
-                            });
-
-                            // Menambahkan permintaan estimasi ke dalam antrian
-                            RequestQueue estimationQueue = Volley.newRequestQueue(requireActivity());
-                            estimationQueue.add(estimationRequest);
-                        }
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    Toast.makeText(getContext(), "Error parsing Profile JSON", Toast.LENGTH_SHORT).show();
-                }
-
-
-                String historyUrl = Db_Contract.getUrlHistory + "?token=" + token.getToken();
-                StringRequest historyRequest = new StringRequest(Request.Method.GET, historyUrl, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String responseHistory) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(responseHistory);
-                            String status = jsonObject.getString("status");
-                            String message = jsonObject.getString("message");
-
-                            JSONArray jsonArray = jsonObject.getJSONArray("data");
-
-                            if (jsonArray.length() > 0) {
-                                JSONObject dataObject = jsonArray.getJSONObject(0);
-                                String enddate = dataObject.optString("end_date", "");
-
-                                String newenddate = convertDateFormat(enddate.substring(0, 10));
-
-                                cycleHistory_id = dataObject.getString("cycleHistory_id");
-                                firstDateTextView.setText(newenddate);
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            Toast.makeText(getContext(), "Error parsing History JSON", Toast.LENGTH_SHORT).show();
-                        }
-                    } //done
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                        Toast.makeText(getContext(), "Error in History request: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                queue.add(historyRequest);
-            }
-
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-                Toast.makeText(getContext(), "Error in Profile request: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> paramV = new HashMap<>();
-                paramV.put("token", token.getToken());
-                return paramV;
-            }
-        };
-
-        // Menambahkan permintaan profil ke dalam antrian
-        queue.add(profileRequest);
-
-        // Return the inflated view
-        return view;
-
+       return view;
     }
 
 
@@ -295,6 +142,92 @@ public class HomeFragment extends Fragment {
             e.printStackTrace();
             return inputDate; // Jika gagal, kembalikan tanggal tanpa perubahan
         }
+    }
+
+    private void getHistory(){
+        tokenManager token = new tokenManager(getContext());
+
+        // URL endpoint API get_profile() dengan parameter token yang sesuai
+        String url = Db_Contract.getUrlHistory + "?token=" + token.getToken();
+
+        // Buat request GET menggunakan Volley
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Handle response dari server jika permintaan sukses
+                        try {
+                            String status = response.getString("status");
+                            String message = response.getString("message");
+
+                            if(status.equals("1")){
+                                JSONArray dataArray = response.getJSONArray("data");
+                                JSONObject data = dataArray.getJSONObject(0);
+
+                                StartDateHist = data.getString("start_date");
+                                EndDateHist = data.getString("end_date");
+                            }else {
+                                throw new JSONException(message);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(getContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Tambahkan request ke queue
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void getEst(){
+        tokenManager token = new tokenManager(getContext());
+
+        // URL endpoint API get_profile() dengan parameter token yang sesuai
+        String url = Db_Contract.urlgetEst + "?token=" + token.getToken();
+
+        // Buat request GET menggunakan Volley
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Handle response dari server jika permintaan sukses
+                        try {
+                            String status = response.getString("status");
+                            String message = response.getString("message");
+
+                            if(status.equals("1")){
+                                JSONArray dataArray = response.getJSONArray("data");
+                                JSONObject data = dataArray.getJSONObject(0);
+
+                                StartDateEst = data.getString("start_date");
+                                EndDateEst = data.getString("end_date");
+                            }else {
+                                throw new JSONException(message);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Toast.makeText(getContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Tambahkan request ke queue
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(jsonObjectRequest);
     }
 
 
